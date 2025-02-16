8e2dffd3590dd375855ef551a25ddc93ef2a3e0a
Muhammadkamranlive <Muhammadkamranntu@email.com>
2025-02-16 11:16:46 +0500

    chargeupdated

---
+++ API/API/Migrations/20250216061404_updatedcargesmodel.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class updatedcargesmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChargePercentage",
+                table: "TenantCharges");
+
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "TenantCharges");
+
+            migrationBuilder.DropColumn(
+                name: "MaxChargeAmount",
+                table: "TenantCharges");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ChargePercentage",
+                table: "TenantCharges",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "TenantCharges",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "MaxChargeAmount",
+                table: "TenantCharges",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+    }
+}
