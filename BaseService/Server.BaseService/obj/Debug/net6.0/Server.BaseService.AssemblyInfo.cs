7d3e4eab9ca0c370f1c8a498e9a34eb0179de918
Muhammadkamranlive <Muhammadkamranntu@email.com>
2025-02-18 20:10:41 +0500

    stage issues

--- BaseService/Server.BaseService/obj/Debug/net6.0/Server.BaseService.AssemblyInfo.cs
+++ BaseService/Server.BaseService/obj/Debug/net6.0/Server.BaseService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server.BaseService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2bad5302e515984a86b2070ca74528229055a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd8b91eb889e440393297c9d9ccd321c7113b73b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server.BaseService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server.BaseService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
