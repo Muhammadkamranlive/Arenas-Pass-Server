fd8b91eb889e440393297c9d9ccd321c7113b73b
Muhammadkamranlive <Muhammadkamranntu@email.com>
2025-02-18 10:12:41 +0500

    xvd

--- API/API/obj/Debug/net6.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17f7ea8abcd611014d4809f3eb6a19e13145c2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2bad5302e515984a86b2070ca74528229055a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Middlewares/Server.Middlewares/obj/Release/net6.0/Server.Middlewares.AssemblyInfo.cs
+++ Middlewares/Server.Middlewares/obj/Release/net6.0/Server.Middlewares.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server.Middlewares")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd8db77b2388b3f14d393fe3712223e02df845c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2bad5302e515984a86b2070ca74528229055a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server.Middlewares")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server.Middlewares")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
